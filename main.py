e47dab66c7f0d4311872a0d766a45e2bd990851d
Trac Quang Thinh <tracquangthinh@gmail.com>
2017-12-05 12:38:40 +0700

    JSON web tokens

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import flask
+from flask_jwt import JWT, jwt_required
+import ConfigParser
 import math_function
 from json_respond import json_respond
 
+# load config from config file
+configParser = ConfigParser.RawConfigParser()  
+configParser.read("config")
+
 app = flask.Flask("wi-processing");
+# set secret key to generate token
+app.config['SECRET_KEY'] = configParser.get('key', 'SECRET_KEY')
+
+class User(object):
+    def __init__(self, id):
+        self.id = id
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+# verify user to send token
+def verify(username, password):
+    if not (username and password):
+        return False
+    if configParser.get('user', 'NAME') == username \
+      and configParser.get('user', 'PASSWORD') == password:
+        return User(id=configParser.get('user', 'ID'))
+# identity user
+def identity(payload):
+    user_id = payload['identity']
+    return {"user_id": user_id}
+
+jwt = JWT(app, verify, identity)
+
 @app.route('/')
 def index():
     return "this is index"
 
 @app.route('/convolution', methods=["POST"])
+@jwt_required()
 def convolution():
   if flask.request.method == "POST":
     # get input from the json file
@@ -24,6 +55,7 @@ def convolution():
     return flask.jsonify(result_json)
 
 @app.route('/deconvolution', methods=["POST"])
+@jwt_required()
 def deconvolution():
   if flask.request.method == "POST":
     # get input from the json file
@@ -39,5 +71,5 @@ def deconvolution():
 
     return flask.jsonify(result_json)
 
-app.run()
+app.run(debug=True)
 
--- test.py
+++ test.py
@@ -3,23 +3,33 @@
 import requests
 
 # test convolutional
-def test_convolution():
+def test_convolution(headers):
   dict_to_send = {
       'curve' : [1.1,2.2, 3.3, 4.4],
       'refCurve': [0.1, 1.2, 2.3, 3.4]
     }
-  res = requests.post('http://localhost:5000/convolution', json=dict_to_send)
+  res = requests.post('http://localhost:5000/convolution', json=dict_to_send, headers=headers)
   print res.text
 
 # test deconvolutional
-def test_deconvolution():
+def test_deconvolution(headers):
   dict_to_send = {
       'signal' : [2, 1],
       'divisor': [0, 1, 0, 0, 1, 1, 0, 0]
     }
-  res = requests.post('http://localhost:5000/deconvolution', json=dict_to_send)
+  res = requests.post('http://localhost:5000/deconvolution', json=dict_to_send, headers=headers)
   print res.text
 
 if __name__ == '__main__':
-  test_convolution()
-  test_deconvolution()
+  try:
+    # get token
+    token = requests.post('http://localhost:5000/auth', json={"username":"test","password":"test"})
+    token = token.json()["access_token"]
+    token = "JWT " + token
+    headers = {"Authorization": token}
+    
+    # request with token
+    test_convolution(headers)
+    test_deconvolution(headers)
+  except:
+    print("Failed")
