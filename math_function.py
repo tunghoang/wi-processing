986a008e33ecab60085e914c40b65ebe2fcbb63f
Trac Quang Thinh <tracquangthinh@gmail.com>
2017-12-08 16:45:07 +0700

    Round for results

--- math_function.py
+++ math_function.py
@@ -6,20 +6,28 @@ import numpy as np
 # convolutional
 def conv(inputC, kernel):
   max_len = max(len(inputC), len(kernel))
-  return np.real(np.fft.ifft( np.fft.fft(inputC, max_len)*np.fft.fft(kernel, max_len))).tolist()
+  result = np.real(np.fft.ifft( np.fft.fft(inputC, max_len)*np.fft.fft(kernel, max_len))).tolist()
+  result = [round(result[i], 4) for i in range(len(result))]
+  return result
 
 # deconvolutional
 def deconv(inputC, kernel):
   max_len = max(len(inputC), len(kernel))
-  return np.real(np.fft.ifft( np.fft.fft(inputC, max_len)/np.fft.fft(kernel, max_len))).tolist()
+  result = np.real(np.fft.ifft( np.fft.fft(inputC, max_len)/np.fft.fft(kernel, max_len))).tolist()
+  result = [round(result[i], 4) for i in range(len(result))]
+  return result
 
 # median
 def median(inputC, kernel_size=None):
-  return signal.medfilt(inputC, kernel_size).tolist()
+  result = signal.medfilt(inputC, kernel_size).tolist()
+  result = [round(result[i], 4) for i in range(len(result))]
+  return result
 
 #Savitzky-Golay filter
 def savgol(inputC, window_length, polyorder):
-  return signal.savgol_filter(inputC, window_length, polyorder).tolist()
+  result = signal.savgol_filter(inputC, window_length, polyorder).tolist()
+  result = [round(result[i], 4) for i in range(len(result))]
+  return result
 
 def curve_filter():
   return 0
